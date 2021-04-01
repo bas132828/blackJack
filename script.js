5c3a11fe0f100ee26a90ad723ce49503c1997b53
bas132828 <63089699+bas132828@users.noreply.github.com>
2021-04-01 20:36:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+
+
+
+
+
+
+
+const steamDeals = async function() {
+  try{
+    const resp =  await fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15') 
+  const deals = await resp.json()
+  console.log(deals)
+   const [top1,top2,top3] = deals;
+  console.log(top1,top2,top3)
+
+
+  } catch(err) {
+      console.log(err)
+  }
+  
+}
+steamDeals()
+
